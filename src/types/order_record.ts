4d9143c6842b931c3652de4c0dc2d0639cf1bc1f
Mohamed elkady <mohameda.elqady@gmail.com>
2025-02-12 14:48:19 +0200

    create file for constants, enums for http methods and network status codes, types file for record type

---
+++ src/types/order_record.ts
@@ -0,0 +1,20 @@
+export interface OrderItem {
+    name: string;
+    id: string;
+    price: number;
+    quantity: number;
+}
+
+export type OrderDecision = "reject" | "accept" | "escalate" | null;
+
+export interface OrderRecord {
+    id: string;
+    reason: string;
+    store_name: string;
+    store_logo: string;
+    store_url: string;
+    amount: number;
+    active: boolean;
+    decision: OrderDecision;
+    items: OrderItem[];
+}
---
+++ src/utils/enums.ts
@@ -0,0 +1,14 @@
+export enum HttpMethod {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE'
+}
+
+export enum NetworkStatusCodes {
+    UnAuthorizedUser = 401,
+    BadRequest = 400,
+    ServerInternalError = 500,
+    OK_200 = 200,
+    OK_299 = 299
+}
